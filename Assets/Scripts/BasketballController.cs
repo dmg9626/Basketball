d541444fe15e2f0b0053b6b2be8dbdd40b945ebf
Drew Graham <drewgraham@Drews-MacBook-Pro.local>
2017-06-21 18:01:58 -0400

    ball physics improved
    - ball now moves when you throw it
    - added left/right/bottom screen bounds (top bound disabled for testing)
    - need to refine ball physics and create hoop

---
+++ Assets/Scripts/BasketballController.cs
@@ -0,0 +1,96 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(CircleCollider2D))]
+
+public class BasketballController : MonoBehaviour 
+{
+    Vector2 offset;
+    Vector2 lastPosition;
+    Vector2 currentPosition;
+
+    Vector2 startPosition = new Vector2(0, -2);
+
+    Rigidbody2D basketball;
+
+    public float maxVelocity = 10;
+    float gravity = 2;
+
+    float screenBoundBottom = -8;
+    float screenBoundTop = 12;
+    float screenBoundLeft = -10;
+    float screenBoundRight = 10;
+
+
+    void Start()
+    {
+        basketball = gameObject.GetComponent<Rigidbody2D>();
+        basketball.gravityScale = 0;
+
+        gameObject.transform.position = startPosition;
+        currentPosition = transform.position;
+    }
+
+    void Update()
+    {
+        lastPosition = currentPosition;
+        currentPosition = transform.position;
+
+        if (transform.position.y < screenBoundBottom)
+            Reset();
+        else if (transform.position.x < screenBoundLeft || transform.position.x > screenBoundRight)
+            Reset();
+        
+        /* make ball smaller as it goes away
+        else
+        {
+            if((currentPosition - lastPosition).y > 0F) // ball moving up
+            {
+                gameObject.transform.localScale.Scale(new Vector3(.001, .001));
+            }
+        }
+        */
+    }
+
+    void Reset()
+    {
+        gameObject.transform.position = startPosition;
+        basketball.gravityScale = 0;
+        basketball.velocity = Vector2.zero;
+    }
+
+    void OnMouseDown()
+    {
+        //_screenPoint = Camera.main.WorldToScreenPoint(gameObject.transform.position);
+
+        offset = gameObject.transform.position - Camera.main.ScreenToWorldPoint(
+            new Vector2(Input.mousePosition.x, Input.mousePosition.y));
+
+        basketball.gravityScale = 0; // disable gravity while mouse down
+        basketball.velocity = new Vector2(0,0);
+    }
+
+    void OnMouseDrag()
+    {
+        Vector2 curScreenPoint = new Vector2(Input.mousePosition.x, Input.mousePosition.y);
+        Vector2 curPosition = (Vector2) Camera.main.ScreenToWorldPoint(curScreenPoint) + offset;
+
+        gameObject.transform.position = curPosition;
+
+        //_angle = Vector2.Angle(Vector2.up, gameObject.GetComponent<Rigidbody2D>().velocity);
+    }
+
+    void OnMouseUp()
+    {
+        // get direction of movement in last frame
+        // move object in that direction at velocity
+
+        Vector2 velocity = (currentPosition - lastPosition) * 20;
+        if (velocity.y > maxVelocity)
+            velocity.y = maxVelocity;
+        
+        basketball.velocity = velocity;
+        basketball.gravityScale = gravity; // disable gravity while mouse down
+    }
+}
